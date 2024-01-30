790c9e55853865a4c3149e1d395f4f499aa36b6b
zoegaidmore <156376939+zoegaidmore@users.noreply.github.com>
2024-01-29 22:53:16 -0800

    Update ChatServer.java

--- ChatServer.java
+++ ChatServer.java
@@ -7,11 +7,14 @@ class Handler implements URLHandler {
 
   public String handleRequest(URI url) {
     if (url.getPath().equals("/add-message")) {
-      String[] parameters = url.getQuery().split("=");
-      if(parameters.length == 2 && parameters[0].equals("s") && parameters[1].equals("user")) {
-        return String.format("%s: %s", parameters[1], parameters[0]);
+      String[] parameters = url.getQuery().split("&");
+      
+      if(parameters.length == 2) {
+        String[] parameter1 = parameters[0].split("=");
+        String[] parameter2 = parameters[1].split("=");
+          if(parameter1[0].equals("s") && parameter2[0].equals("user")) {
+            return String.format("%1$s: %2$s", parameter1[1], parameter2[1]);
+          }
       }
-    }
-    else {
     }
 }
